2ba0864ae0b06cc44918b8cf95379968f7e3d3bb
Erik Westrup <erik.westrup@gmail.com>
2022-01-18 22:10:52 +0100

    Remove automatic /home/user\*/.backup_exclude feature
    As discussed in #45, this aims at solving a problem larger than the
    scope of this project. Thus simplify and remove.

--- usr/local/sbin/restic_backup.sh
+++ usr/local/sbin/restic_backup.sh
@@ -21,21 +21,6 @@ exit_hook() {
 }
 trap exit_hook INT TERM
 
-## Additonal service: if /home is being backed up, allow user specify backup files in home directory or default $XDG_CONFIG_HOME path
-exclusion_args_from_homedirs() {
-	local homeroot="$1"
-	local args=""
-	if [[ $BACKUP_PATHS == *"$homeroot"* ]]; then
-		for homedir in $homeroot/*; do
-			excl_home="$homedir/.backup_exclude"
-			excl_xdg="$homedir/.config/restic/backup_exclude"
-			test -f $excl_home && args+=" --exclude-file $excl_home"
-			test -f $excl_xdg && args+=" --exclude-file $excl_xdg"
-		done
-	fi
-	echo "$args"
-}
-
 # Remove duplicate entries by sorting them
 sort_unique() {
   local paths="$1"
@@ -43,20 +28,17 @@ sort_unique() {
   echo $(tr ' ' '\n' <<< "$paths" | sort -u | tr '\n' ' ' | xargs)
 }
 
-# Set up exclude files: global + path-specific ones + home directories.
+# Set up exclude files: global + path-specific ones
 # NOTE that restic will fail the backup if not all listed --exclude-files exist. Thus we should only list them if they are really all available.
 ##  Global backup configuration.
 exclusion_args="--exclude-file ${RESTIC_BACKUP_EXCLUDE}"
-## Self-contained backup files per backup path. E.g. having an USB disk at /mnt/media in BACKUP_PATHS, then it can have a /mnt/media/.backup_exclude
+## Self-contained backup files per backup path. E.g. having an USB disk at /mnt/media in BACKUP_PATHS,
+# a file /mnt/media/.backup_exclude will automatically be detected and used:
 for backup_path in ${BACKUP_PATHS[@]}; do
 	if [ -f "$backup_path/.backup_exclude" ]; then
 		exclusion_args+=" --exclude-file $backup_path/.backup_exclude"
 	fi
 done
-exclusion_args+="$(exclusion_args_from_homedirs /home)"
-## And the same service of macOS users, having /Users instead of /home
-exclusion_args+="$(exclusion_args_from_homedirs /Users)"
-exclusion_args=$(sort_unique "$exclusion_args")  # remove duplicate entries caused by having /home/user as backup path
 
 # NOTE start all commands in background and wait for them to finish.
 # Reason: bash ignores any signals while child process is executing and thus the trap exit hook is not triggered.
